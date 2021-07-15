6496a33e980c7e1ec92ac55b8e352da3a36d68cf
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-07-15 14:46:53 -0400

    Form Validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -26,37 +26,48 @@ $(document).ready(function() {
     return output
   };
   const renderTweets = tweetsArray => {
+    $("#tweets-container").empty();
     for (const key in tweetsArray) {
       const $tweet = createTweetElement(tweetsArray[key]);
-      $('#tweets-container').append($tweet);
+      $('#tweets-container').prepend($tweet);
     }
   }
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET' 
+    })
+    .then(function (data) {
+        renderTweets(data);
+    })
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  }
+  loadTweets();
 
-  renderTweets(data);
+  $("#form_submit").submit(function(event) {
+     // prevent the default behaviour to leave the page
+    event.preventDefault();
+    const inputLength = $(this).children('textarea').val().length;
+    if(inputLength >140) {                                     
+      $(this).each(function() {
+        alert("Sorry, content exceeds the 140 character limit");
+      });
+    } else if (inputLength === 0) {                            
+        $(this).each(function() {
+          alert("Tweet is empty");
+        });
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: $(this).serialize()
+      }).then(function(data) {
+         loadTweets();
+      })
+      $(this).children('textarea').val('');
+      $(this).find(".counter").text('140');                               
 
+  }
+
+  });
 });
